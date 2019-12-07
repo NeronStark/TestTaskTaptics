bdf47931a89e6eb8d42c730c876f5dadf3ad8546
NeronStark <allayarovtimur@yandex.ru>
2019-12-07 22:23:11 +0500

    Add ScriptBase (Also make Simple Timer)

---
+++ Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    private float _Time = 5;
+    private float extraTime;
+    public float ExtraTime { get => extraTime; set => extraTime = value; }
+    bool isTimeRun = true;
+
+
+
+    private void Update()
+    {
+        if(isTimeRun)
+        {
+            if(_Time > 0)
+            {
+                _Time -= Time.deltaTime;
+            }
+            else
+            {
+                isTimeRun = false;
+                print("Time is Out");
+            }
+        }
+    }
+}
